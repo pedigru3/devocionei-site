b8a930f650ec00fa2c61a04763b6b767e3b8896c
pedigru3 <contosdeferreira@hotmail.com>
2024-09-25 10:58:38 -0300

    otmize image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import Logo from "@/images/devocionei.png"
+import Logo from "@/images/devocionei.webp"
 
 export default function Home() {
-
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8">
@@ -21,6 +20,7 @@ export default function Home() {
             alt="Logo Devocionei"
             width={250}
             height={250}
+            priority
           />
         <p className="text-center mb-6">
           Com o Devocionei, você pode inserir seu versículo e receber perguntas que ajudam na reflexão e aprendizado.
@@ -44,14 +44,14 @@ export default function Home() {
               xmlns="http://www.w3.org/2000/svg" 
               fill="none" 
               viewBox="0 0 24 24" 
-              stroke-width="1.5" 
+              strokeWidth="1.5" 
               stroke="currentColor" 
               className="w-12 h-12 text-primary"
             >
               {/* Ícone representando o benefício 1 */}
               <path 
-                stroke-linecap="round" 
-                stroke-linejoin="round" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
                 d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A48.117 48.117 0 0012 20.5c1.052 0 2.062-.18 3-.512V6.042z" 
               />
             </svg>
@@ -67,14 +67,14 @@ export default function Home() {
               xmlns="http://www.w3.org/2000/svg" 
               fill="none" 
               viewBox="0 0 24 24" 
-              stroke-width="1.5" 
+              strokeWidth="1.5" 
               stroke="currentColor" 
               className="w-12 h-12 text-primary"
             >
               {/* Ícone representando o benefício 2 */}
               <path 
-                stroke-linecap="round" 
-                stroke-linejoin="round" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
                 d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125m-1.687 1.688l-1.687 1.688" 
               />
             </svg>
@@ -90,14 +90,14 @@ export default function Home() {
               xmlns="http://www.w3.org/2000/svg" 
               fill="none" 
               viewBox="0 0 24 24" 
-              stroke-width="1.5" 
+              strokeWidth="1.5" 
               stroke="currentColor" 
               className="w-12 h-12 text-primary"
             >
               {/* Ícone representando o benefício 3 */}
               <path 
-                stroke-linecap="round" 
-                stroke-linejoin="round" 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" 
               />
             </svg>
