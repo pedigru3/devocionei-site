308169b06c388c84117f19f73b12f08611fa8846
pedigru3 <contosdeferreira@hotmail.com>
2025-04-05 02:09:37 -0300

    change to client redirect

--- src/app/joinGroup/[slug]/page.tsx
+++ src/app/joinGroup/[slug]/page.tsx
@@ -1,26 +1,20 @@
-import { redirect } from 'next/navigation';
+"use client";
+
+import { useEffect } from "react";
 
 export default function JoinGroup({ params }: { params: { slug: string } }) {
-  // Você pode adicionar lógica aqui para verificar se o usuário está em um dispositivo móvel
-  // e redirecionar para o URL Scheme se necessário
-  
-  // Exemplo de redirecionamento para o URL Scheme
-  const urlScheme = `devocioneiplus://devocioneiplus.com/joinGroup/${params.slug}`;
-  
-  // Redireciona para o URL Scheme
-  redirect(urlScheme);
-  
-  // Se o redirecionamento não funcionar, você pode mostrar uma página com um botão
+  const urlScheme = `devocioneiplus://joinGroup/${params.slug}`;
+
+  useEffect(() => {
+    window.location.href = urlScheme;
+  }, [urlScheme]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8">
       <h1 className="text-2xl font-bold mb-4">Abrir no App Devocionei</h1>
-      <a 
-        href={urlScheme}
-        className="bg-primary text-white px-6 py-3 rounded-full"
-      >
+      <a href={urlScheme} className="bg-primary text-white px-6 py-3 rounded-full">
         Abrir no App
       </a>
     </div>
   );
 }
-
