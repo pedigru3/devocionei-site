3e8c4d76b01a57911854aba2327ee41aacb1cfd1
Felipe Ferreira <ferreira.contato1@gmail.com>
2025-05-26 12:27:22 -0300

    changes images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     siteName: "BibleRats",
     images: [
       {
-        url: "/og=image.png",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "BibleRats - Desafio devocional",
